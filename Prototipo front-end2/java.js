695fc147ddd093b3ce257faabea69da7d897c423
Anderson Vieira <42439545+Anddersson@users.noreply.github.com>
2019-05-06 10:33:00 -0300

    Add files via upload

---
+++ Prototipo front-end2/java.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("#tabela input").keyup(function(){        
+        var index = $(this).parent().index();
+        var nth = "#tabela td:nth-child("+(index+1).toString()+")";
+        var valor = $(this).val().toUpperCase();
+        $("#tabela tbody tr").show();
+        $(nth).each(function(){
+            if($(this).text().toUpperCase().indexOf(valor) < 0){
+                $(this).parent().hide();
+            }
+        });
+    });
+ 
+    $("#tabela input").blur(function(){
+        $(this).val("");
+    }); 
+
+    $('#minor_partners').on('click', 'a',function() {
+        $(this).attr('target', '_blank');
+    });
+
+    
+});
+
